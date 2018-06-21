fe62f522f89f9d26262153ffbc92165abce854f0
JesusSotoT <soto.tovar.jesus@gmail.com>
2018-06-21 17:31:49 -0500

    Cargando Core +

---
+++ resources/views/login.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>::Admin Login::</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{asset('assets/img/favicon.ico')}}" />
+    <!-- Bootstrap -->
+    <!-- global css -->
+    <link href="{{asset('assets/css/app.css')}}" rel="stylesheet">
+    <!-- end of global css -->
+    <!--page level css -->
+    <link rel="stylesheet" href="{{asset('assets/vendors/iCheck/css/all.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/custom.css')}}">
+    <link href="{{asset('assets/css/login2.css')}}" rel="stylesheet">
+    <!--end page level css-->
+</head>
+
+<body class="bg-slider">
+<div class="preloader">
+    <div class="loader_img"><img src="{{asset('assets/img/loader.gif')}}" alt="loading..." height="64" width="64"></div>
+</div>
+<div class="container">
+    <div class="row " id="form-login">
+        <div class="col-md-8  col-sm-8  col-10 mx-auto login-content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="header">
+                        <h2 class="text-center">
+                            Login
+                            <small> with</small>
+                            <img src="{{asset('assets/img/pages/logo.png')}}" alt="logo">
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row row-bg-color">
+                <div class="col-lg-8 col-12 core-login">
+                    <form  method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="form-group ">
+                                   <label for="email" class="control-label">{{ __('E-Mail Address') }}</label>
+
+                                     <div class="input-group">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                         </div>
+                 </div>
+         </div>
+</div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="form-group ">
+                                     <label for="password" class="control-label">{{ __('Password') }}</label>
+                                    <div class="input-group">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                       </div>                   
+               </div>
+       </div>
+</div>
+                        <div class="form-group">
+                             <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            <a href="{{ route('password.request') }}" id="forgot" class="text-primary forgot1  pull-right">  {{ __('Forgot Your Password?') }}</a>
+                        </div>
+                        <div class="form-group ">
+                             <button type="submit" class="btn btn-lg btn-success" style="padding-left: 20px;">
+                                    {{ __('Login') }}
+                                </button>
+                            <br>
+                            <hr>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- global js -->
+<script src="{{asset('assets/js/jquery.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/backstretch.js')}}"></script>
+<!-- end of global js -->
+<!-- page level js -->
+<script type="text/javascript" src="{{asset('assets/vendors/iCheck/js/icheck.js')}}"></script>
+<script src="{{asset('assets/vendors/bootstrapvalidator/js/bootstrapValidator.min.js')}}" type="text/javascript"></script>
+<script type="text/javascript" src="{{asset('assets/js/custom_js/login.js')}}"></script>
+<!-- end of page level js -->
+</body>
+
+</html>
